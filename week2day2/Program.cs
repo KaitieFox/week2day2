13a764ef2e9c06e12778d8e8ceb67700c90e7b7c
KaitieFox <kaitiefox@gmail.com>
2017-10-03 15:14:18 -0400

    For Foreach

--- week2day2/Program.cs
+++ week2day2/Program.cs
@@ -43,19 +43,66 @@ namespace week2day2
 
             //Console.WriteLine("");
 
-            //Let's loop with strings
-            string greeting = "My name is Little Bill";
-            string[] words = greeting.Split(); //makes it a string array.
+            ////Let's loop with strings
+            //string greeting = "My name is Little Bill";
+            //string[] words = greeting.Split(); //makes it a string array.
+
+            //for(int i = 0; i < words.Length; i++)
+            //{
+            //    Console.Write(words[i] + " ");
+            //}
+            //Console.WriteLine("\n");
+
+            ////do it
+
+            //string[] days = { "Monday", "Tuesday", "Wednesday" };
+            //for (int i = 0; i < days.Length; i++)
+            //{
+            //    Console.WriteLine(days[i] + " ");
+            //}
+
+            //string begin = "Once upon a time";
+            //string[] storyWords = begin.Split();
+            //Array.Reverse(storyWords);
+
+            //for (int i = 0; i < storyWords.Length; i++)
+            //{
+            //    Console.WriteLine(storyWords[i]);
+            //}
+
+            //Console.WriteLine(" ");
+
+            //string[] months = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec" };
+            ////type must match. then month is temp varaible. Make sure it's not a variable you've already used.
+            //foreach(string month in months)
+            //{
+            //    Console.WriteLine(month);
+            //}
+            ////this will print out all the months.
+
+            //string[] bands = { "Queen", "Anthony Santos", "Glass Animals", "Bon Iver", "Bonobo" };
+            //Console.Write("My favorite bands are ");
+            //foreach (string band in bands)
+            //{
+            //    Console.Write(band + " and ");
+            //}
+            ////note what's going on 
+
+
+            //do it
+            string[] foods = { "aloo gobi", "burgers", "tacos", "coffee" };
             
-            for(int i = 0; i < words.Length; i++)
+            foreach (string food in foods)
             {
-                Console.Write(words[i] + " ");
+                Console.WriteLine(food);
             }
-            Console.WriteLine("\n");
 
-            
+            int[] lucky = { 3, 64, 13, 7 };
 
-
+            foreach (int luck in lucky)
+            {
+                Console.WriteLine("Your lucky number is: " + luck);
+            }
 
 
 
