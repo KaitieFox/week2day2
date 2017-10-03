edbe739ff5f9069e104c34ff40a7098e3a2229db
KaitieFox <kaitiefox@gmail.com>
2017-10-03 13:51:42 -0400

    Loop Int

--- week2day2/Program.cs
+++ week2day2/Program.cs
@@ -10,6 +10,57 @@ namespace week2day2
     {
         static void Main(string[] args)
         {
+            //for(int i = 0; i <= 10; i+=2) // that writes up, skips a number
+            //{
+            //   Console.WriteLine(i);                
+            //}
+            //for (int counter = 50; counter <= 100; counter++)
+            //{
+            //    Console.WriteLine(counter);
+            //}
+
+            //string greetings = "My name is Jordan";
+            //string[] words = greetings.Split();
+
+            //for (int i = 0; i < words.Length; i++)
+            //{
+            //    Console.WriteLine(words[i]);
+            //} 
+            /////that was with Jordan.
+
+            //Okay, walking through this again.
+
+
+            //for (int i = 1; i <= 10; i++) //generally just used within this for loop.
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int counter = 50; counter <= 100; counter++)
+            //{
+            //    Console.Write(counter + " "); //can put it on the same line. Make sure you concatenate an empty string
+            //}
+
+            //Console.WriteLine("");
+
+            //Let's loop with strings
+            string greeting = "My name is Little Bill";
+            string[] words = greeting.Split(); //makes it a string array.
+            
+            for(int i = 0; i < words.Length; i++)
+            {
+                Console.Write(words[i] + " ");
+            }
+            Console.WriteLine("\n");
+
+            
+
+
+
+
+
+
+
         }
     }
 }
